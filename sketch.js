a1117510e3b846c1c88d59e85cf80d926b666464
CODMaker12 <156646781+CODMaker12@users.noreply.github.com>
2024-01-15 18:17:36 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var box; 
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(30, 84, 67, 83)
+
+}
+
+function draw() 
+{
+  background(30);
+  drawSprites(); 
+  if (keyIsDown(RIGHT_ARROW)) {
+  box.x = box.x + 5
+
+  
+  }
+  if (keyIsDown(LEFT_ARROW)) {
+  box.x = box.x - 5
+  }
+}
+
+
+
+
